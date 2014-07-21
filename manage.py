4cfc633e19147fe4c300acf6f6678f612d84f672
Fabio Souto <fsoutomoure@gmail.com>
2014-07-21 13:55:27 +0200

    Added apps to the path

--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-import os, sys
+import os
+import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
     # Add the lib/ directory to the path
     sys.path.append("lib")
+    sys.path.append("apps")
 
     from django.core.management import execute_from_command_line
 
