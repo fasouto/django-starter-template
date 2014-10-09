b068f9b91020fc2f51f3862f668244b28cde9556
Fabio Souto <fsoutomoure@gmail.com>
2014-10-09 03:28:36 +0200

    Better fable

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,32 @@
 """
 Basic fabfile for django projects
 """
-from fabric.api import *
+import posixpath
+
+from fabric.api import run, local, env, settings, cd, task
+from fabric.contrib.files import exists
+from fabric.operations import _prefix_commands, _prefix_env_vars, sudo
+
+# Set to true if you can restart your webserver (via wsgi.py), false to stop/start your webserver
+DJANGO_SERVER_RESTART = False
+
+# EDIT THIS INFORMATION
+env.hosts = ['']
+env.password = ''
+env.code_dir = ''
+env.project_dir = ''
+env.static_root = ''
+env.virtualenv = ''
+env.code_repo = ''
+env.django_settings_module = ''
+
+# Python version
+PYTHON_BIN = "python2.7"
+PYTHON_PREFIX = ""  # e.g. /usr/local  Use "" for automatic
+PYTHON_FULL_PATH = "%s/bin/%s" % (PYTHON_PREFIX, PYTHON_BIN) if PYTHON_PREFIX else PYTHON_BIN
+
+
+
 
 def virtualenv(venv_dir):
     """
@@ -9,42 +34,88 @@ def virtualenv(venv_dir):
     """
     return settings(venv=venv_dir)
 
+
 def run_venv(command, **kwargs):
     """
     Runs a command in a virtualenv (which has been specified using
     the virtualenv context manager
     """
-    run("source %s/bin/activate" % env.venv + " && " + command, **kwargs)
+    run("source %s/bin/activate" % env.virtualenv + " && " + command, **kwargs)
+
 
 def install_dependencies():
     ensure_virtualenv()
-    with virtualenv(VENV_DIR):
-        with cd(SRC_DIR):
+    with virtualenv(env.virtualenv):
+        with cd(env.code_dir):
             run_venv("pip install -r requirements/production.txt")
 
+
 def ensure_virtualenv():
-    if exists(VENV_DIR):
+    if exists(env.virtualenv):
         return
 
-    with cd(DJANGO_APP_ROOT):
+    with cd(env.code_dir):
         run("virtualenv --no-site-packages --python=%s %s" %
-            (PYTHON_BIN, VENV_SUBDIR))
+            (PYTHON_BIN, env.virtualenv))
         run("echo %s > %s/lib/%s/site-packages/projectsource.pth" %
-            (SRC_DIR, VENV_SUBDIR, PYTHON_BIN))
+            (env.project_dir, env.virtualenv, PYTHON_BIN))
+
+def ensure_src_dir():
+    if not exists(env.code_dir):
+        run("mkdir -p %s" % env.code_dir)
+    with cd(env.code_dir):
+        if not exists(posixpath.join(env.code_dir, '.git')):
+            run('git clone %s .' % (env.code_repo))
+
+
+@task
+def push_sources():
+    """
+    Push source code to server
+    """
+    ensure_src_dir()
+    local('git push origin master')
+    with cd(env.code_dir):
+        run('git pull origin master')
+
+
+@task
+def run_tests():
+    """ Runs the Django test suite as is.  """
+    local("./manage.py test")
+
+
+@task
+def version():
+    """ Show last commit to the deployed repo. """
+    with cd(env.code_dir):
+        run('git log -1')
+
+
+@task
+def test():
+    """ 
+    Prints information about the host. 
+    Use it to check if env configuration is ok.
+    """
+    run("uname -a")
+
 
 @task
 def webserver_stop():
     """
     Stop the webserver that is running the Django instance
     """
-    run("service apache2 stop")
+    sudo("service apache2 stop")
+
 
 @task
 def webserver_start():
     """
     Starts the webserver that is running the Django instance
     """
-    run("service apache2 start")
+    sudo("service apache2 start")
+
 
 @task
 def webserver_restart():
@@ -58,3 +129,81 @@ def webserver_restart():
         with settings(warn_only=True):
             webserver_stop()
         webserver_start()
+
+
+def restart():
+    """ Restart the wsgi process """
+    with cd(env.code_dir):
+        run("touch %s/glance/wsgi.py" % env.code_dir)
+
+
+def collectstatic():
+    assert env.static_root.strip() != '' and env.static_root.strip() != '/'
+    with virtualenv(env.virtualenv):
+        with cd(env.code_dir):
+            run_venv("./manage.py collectstatic --clear --noinput")
+
+    run("chmod -R ugo+r %s" % env.static_root)
+
+
+@task
+def first_deployment_mode():
+    """
+    Use before first deployment to switch on fake south migrations.
+    """
+    env.initial_deploy = True
+
+
+@task
+def south_migrations(app=None):
+    """
+    Run the south migrations to the database
+    Usage: fab south_migrations:app_name
+    """
+    with virtualenv(env.virtualenv):
+        with cd(env.code_dir):
+            if getattr(env, 'initial_deploy', False):
+                run_venv("./manage.py syncdb --all")
+                run_venv("./manage.py migrate --fake --noinput")
+            else:
+                run_venv("./manage.py syncdb --noinput")
+                if app:
+                    run_venv("./manage.py migrate %s --noinput" % app)
+                else:
+                    run_venv("./manage.py migrate --noinput")
+
+
+@task
+def sshagent_run(cmd):
+    """
+    Helper function.
+    Runs a command with SSH agent forwarding enabled.
+
+    Note:: Fabric (and paramiko) can't forward your SSH agent.
+    This helper uses your system's ssh to do so.
+    """
+    # Handle context manager modifications
+    wrapped_cmd = _prefix_commands(_prefix_env_vars(cmd), 'remote')
+    try:
+        host, port = env.host_string.split(':')
+        return local(
+            "ssh -p %s -A %s@%s '%s'" % (port, env.user, host, wrapped_cmd)
+        )
+    except ValueError:
+        return local(
+            "ssh -A %s@%s '%s'" % (env.user, env.host_string, wrapped_cmd)
+        )
+
+
+@task
+def deploy():
+    """
+    Deploy the project.
+    """
+    with settings(warn_only=True):
+        webserver_stop()
+    push_sources()
+    install_dependencies()
+    south_migrations()
+    collectstatic()
+    webserver_start()
