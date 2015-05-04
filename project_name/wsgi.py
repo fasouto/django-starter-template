318e680562bb6cfae49d101ce1d45ea6c3754583
Fabio Souto <fsoutomoure@gmail.com>
2015-05-05 01:24:56 +0200

    Point the settings to production

--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -49,7 +49,7 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__) + "../../")
 # Add the app code to the path
 #sys.path.append(PROJECT_ROOT)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings.production")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
