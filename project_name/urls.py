42f12097e45f7a231c5818dbac720d2ec2269305
teratzu <teratzu@gmail.com>
2018-07-08 01:15:35 +0200

    missing method

--- project_name/urls.py
+++ project_name/urls.py
@@ -2,6 +2,7 @@
 
 from django.conf import settings
 from django.conf.urls import include
+from django.urls import path
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import index, sitemap
