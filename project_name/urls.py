9215a5fa98f5659ed8ab8f4e92c6d910d053705c
teratzu <teratzu@gmail.com>
2018-07-08 01:13:03 +0200

    URLS fixes

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,7 +1,7 @@
 """ Default urlconf for {{ project_name }} """
 
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import index, sitemap
@@ -16,18 +16,18 @@ sitemaps = {
 }
 
 urlpatterns = [
-    url(r'', include('base.urls')),
+    path('', include('apps.base.urls')),
 
     # Admin
-    url(r'^admin/', admin.site.urls),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    path('admin/', admin.site.urls),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Sitemap
-    url(r'^sitemap\.xml$', index, {'sitemaps': sitemaps}),
-    url(r'^sitemap-(?P<section>.+)\.xml$', sitemap, {'sitemaps': sitemaps}),
+    path(r'sitemap\.xml', index, {'sitemaps': sitemaps}),
+    path(r'sitemap-<section:section>\.xml', sitemap, {'sitemaps': sitemaps}),
 
     # robots.txt
-    url(r'^robots\.txt$',
+    path(r'robots\.txt',
         TemplateView.as_view(
             template_name='robots.txt',
             content_type='text/plain')
@@ -37,7 +37,7 @@ urlpatterns = [
 if settings.DEBUG:
     # Add debug-toolbar
     import debug_toolbar  # noqa
-    urlpatterns.append(url(r'^__debug__/', include(debug_toolbar.urls)))
+    urlpatterns.append(path('^__debug__/', include(debug_toolbar.urls)))
 
     # Serve media files through Django.
     urlpatterns += static(settings.STATIC_URL,
@@ -47,13 +47,7 @@ if settings.DEBUG:
 
     # Show error pages during development
     urlpatterns += [
-<<<<<<< HEAD
         path('403/', permission_denied),
         path('404/', page_not_found),
         path('500/', server_error)
-=======
-        url(r'^403/$', permission_denied),
-        url(r'^404/$', page_not_found),
-        url(r'^500/$', server_error)
->>>>>>> parent of a6a546a... Changes to BASE_DIR to point to the right directory and django.urls 'url' to 'path'.
     ]
